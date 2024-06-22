1bd8d91d9beb8c5187168492e1ec926b8cf93c0d
$ʜɪᴠɢᴜɴ <102505925+ShivgunGaming@users.noreply.github.com>
2024-06-22 10:26:06 +1000

    Update getHelpMessage.js

--- utils/getHelpMessage.js
+++ utils/getHelpMessage.js
@@ -1,27 +1,34 @@
 const { EmbedBuilder } = require("discord.js");
 
+// Function to get the usage details for a specific command
+const getCommandUsage = (command, description, usage, example = null) => `
+  **${command}**
+  ${description}
+  \`\`\`
+  Usage: ${usage}
+  ${example ? `Example: ${example}` : ""}
+  \`\`\`
+`;
+
 // Create and return an EmbedBuilder object for the help message
-const getHelpMessage = () => new EmbedBuilder()
-  .setTitle("Help")
-  .setDescription(`
+const getHelpMessage = () => {
+  const commandsList = `
     **Available Commands:**
     - \`/verify\`: Verify your Bitcoin inscription.
     - \`/help\`: Get help with bot commands.
+  `;
 
+  const commandDetails = `
     **Command Details:**
-    **/verify**
-    Use this command to verify your Bitcoin inscription.
-    \`\`\`
-    Usage: /verify <address>
-    Example: /verify bc1p...0lhx
-    \`\`\`
-    **/help**
-    Use this command to display this help message.
-    \`\`\`
-    Usage: /help
-    \`\`\`
-  `)
-  .setColor("#ED9121")
-  .setTimestamp();
+    ${getCommandUsage("/verify", "Use this command to verify your Bitcoin inscription.", "/verify <address>", "bc1p...0lhx")}
+    ${getCommandUsage("/help", "Use this command to display this help message.", "/help")}
+  `;
+
+  return new EmbedBuilder()
+    .setTitle("Help")
+    .setDescription(`${commandsList}${commandDetails}`)
+    .setColor("#ED9121")
+    .setTimestamp();
+};
 
 module.exports = { getHelpMessage };
