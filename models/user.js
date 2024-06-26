846b40d640ad4f3bcab53a3b249fb36b2b5aba71
$ʜɪᴠɢᴜɴ <102505925+ShivgunGaming@users.noreply.github.com>
2024-06-26 23:13:39 +1000

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,41 +1,49 @@
 const { Sequelize, DataTypes } = require("sequelize");
 
-// Initialize Sequelize with SQLite dialect and specify database file
-const sequelize = new Sequelize({
-  dialect: "sqlite",
-  storage: "database.sqlite"
-});
+const initializeSequelize = () => {
+  return new Sequelize({
+    dialect: "sqlite",
+    storage: "database.sqlite",
+  });
+};
 
-// Define User model
-const User = sequelize.define("User", {
-  discordId: {
-    type: DataTypes.STRING,
-    primaryKey: true
-  },
-  bitcoinAddress: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  inscriptionId: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  otp: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-});
+const defineUserModel = (sequelize) => {
+  return sequelize.define("User", {
+    discordId: {
+      type: DataTypes.STRING,
+      primaryKey: true,
+    },
+    bitcoinAddress: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    inscriptionId: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    otp: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
+};
 
-// Synchronize the model with the database
-(async () => {
+const synchronizeDatabase = async (sequelize) => {
   try {
     await sequelize.authenticate();
     console.log("Connection to the database has been established successfully.");
-    await sequelize.sync(); // This synchronizes all defined models with the database
+    await sequelize.sync();
     console.log("Database synchronized.");
   } catch (error) {
     console.error("Unable to connect to the database:", error);
   }
+};
+
+const sequelize = initializeSequelize();
+const User = defineUserModel(sequelize);
+
+(async () => {
+  await synchronizeDatabase(sequelize);
 })();
 
 module.exports = { User, sequelize };
